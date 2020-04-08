55eb160108cf3a37878513a862e7815b45f4a276
Myp3ukkk <58299231+Myp3ukkk@users.noreply.github.com>
2020-04-08 14:22:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,21 +18,21 @@ bot.music.start(bot, {
 });
 
 // Events.
-bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: `k!help | ${client.guilds.size}`, type: 0 } });
-    bot.user.setStatus('idle');
+client.on('ready', () => {
+    client.user.setPresence({ game: { name: `k!help | ${client.guilds.size}`, type: 0 } });
+    client.user.setStatus('idle');
 });
 
-bot.on("guildCreate", guild => {
+client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`Меня добавили на ${guild.name} (id: ${guild.id})`);
-  bot.user.setPresence({ game: { name: `k!help | ${client.guilds.size}`, type: 0 } });
+  client.user.setPresence({ game: { name: `k!help | ${client.guilds.size}`, type: 0 } });
 });
 
-bot.on("guildDelete", guild => {
+client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`Меня удалили с ${guild.name} (id: ${guild.id})`);
-  bot.user.setPresence({ game: { name: `k!help | ${client.guilds.size}`, type: 0 } });
+  client.user.setPresence({ game: { name: `k!help | ${client.guilds.size}`, type: 0 } });
 });
 
 // Bot Login.
